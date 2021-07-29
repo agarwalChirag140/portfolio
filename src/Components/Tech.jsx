26f21e391937cfcf20d2c96662d36a7660e54287
agarwalchirag <agarwalchirag140@gmail.com>
2021-07-30 02:24:49 +0530

    Some Modification in Tech Stack Part

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -10,63 +10,63 @@ export const Tech = () => {
     return (
         <>
             <p className="text-center text-4xl font-bold text-gray-500 mt-3">Tech Stack</p>
-            <div className="grid grid-cols-2 grid-rows-2 sm:flex m-auto sm:justify-evenly w-full sm:w-3/4 ">
+            <div className="grid grid-cols-2 grid-rows-2 sm:flex m-auto sm:justify-evenly w-full sm:w-4/5 mt-10">
 
                 <div className="m-auto mt-2">
-                    <div className="w-32 h-32 sm:w-40 sm:h-40 rounded-full p-6 bg-green-700 ">
-                        <div className="w-20 h-20 sm:w-28 sm:h-28 rounded-full bg-white flex justify-center items-center">
+                    <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
+                        <div className="w-24 h-24 sm:w-28 sm:h-28 rounded-full bg-white flex justify-center items-center">
                             <img src={html} alt="html logo" className="w-16" />
                         </div>
                     </div>
-                    <p className="text-center pt-2 text-lg">HTML5</p>
+                    <p className="text-center pt-2 text-lg font-bold">HTML5</p>
                 </div>
 
                 <div className="m-auto">
-                    <div className="w-32 h-32 sm:w-40 sm:h-40 rounded-full p-6 bg-green-700 ">
-                        <div className="w-20 h-20 sm:w-28 sm:h-28  rounded-full bg-white flex justify-center items-center">
+                    <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
+                        <div className="w-24 h-24 sm:w-28 sm:h-28  rounded-full bg-white flex justify-center items-center">
                             <img src={CSS} alt="css logo" className="w-12" />
                         </div>
                     </div>
-                    <p className="text-center pt-2 text-lg">CSS3</p>
+                    <p className="text-center pt-2 text-lg font-bold">CSS3</p>
                 </div>
 
                 <div className="m-auto">
-                    <div className="w-32 h-32 sm:w-40 sm:h-40 rounded-full p-6 bg-green-700 ">
-                        <div className="w-20 h-20 sm:w-28 sm:h-28  rounded-full bg-white">
+                    <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
+                        <div className="w-24 h-24 sm:w-28 sm:h-28  rounded-full bg-white">
                             <img src={JavaScript} alt="js logo" />
                         </div>
                     </div>
-                    <p className="text-center pt-2 text-lg">JAVASCRIPT</p>
+                    <p className="text-center pt-2 text-lg font-bold">JAVASCRIPT</p>
                 </div>
 
                 <div className="m-auto">
-                    <div className="w-32 h-32 sm:w-40 sm:h-40 rounded-full p-6 bg-green-700 ">
-                        <div className="w-20 h-20 sm:w-28 sm:h-28  rounded-full bg-white">
+                    <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
+                        <div className="w-24 h-24 sm:w-28 sm:h-28  rounded-full bg-white">
                             <img src={ReactJS} alt="react js logo" className="p-2" />
                         </div>
                     </div>
-                    <p className="text-center pt-2 text-lg">REACT JS</p>
+                    <p className="text-center pt-2 text-lg font-bold">REACT JS</p>
                 </div>
             </div>
 
             <div className="grid grid-cols-2 grid-rows-1 sm:flex m-auto sm:justify-evenly w-full sm:w-2/5 mt-4">
 
                 <div className="m-auto mt-2">
-                    <div className="w-32 h-32 sm:w-40 sm:h-40 rounded-full p-6 bg-green-700 ">
-                        <div className="w-20 h-20 sm:w-28 sm:h-28 rounded-full bg-white flex justify-center items-center">
+                    <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
+                        <div className="w-24 h-24 sm:w-28 sm:h-28 rounded-full bg-white flex justify-center items-center">
                             <img src={Redux} alt="html logo" className="w-16" />
                         </div>
                     </div>
-                    <p className="text-center pt-2 text-lg">REDUX</p>
+                    <p className="text-center pt-2 text-lg font-bold">REDUX</p>
                 </div>
 
                 <div className="m-auto">
-                    <div className="w-32 h-32 sm:w-40 sm:h-40 rounded-full p-6 bg-green-700 ">
-                        <div className="w-20 h-20 sm:w-28 sm:h-28  rounded-full bg-white flex justify-center items-center">
+                    <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
+                        <div className="w-24 h-24 sm:w-28 sm:h-28  rounded-full bg-white flex justify-center items-center">
                             <img src={Tailwind} alt="css logo" className="w-24" />
                         </div>
                     </div>
-                    <p className="text-center pt-2 text-lg">TAILWIND CSS</p>
+                    <p className="text-center pt-2 text-lg font-bold">TAILWIND CSS</p>
                 </div>
 
             </div>
