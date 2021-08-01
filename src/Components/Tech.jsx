c0392df8cb3f62a622311d8ddbe33654ff553f70
agarwalchirag <agarwalchirag140@gmail.com>
2021-08-02 00:17:34 +0530

    Some modification in the tech part

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -9,10 +9,10 @@ import Tailwind from "../Images/Tailwind-removebg-preview.png"
 export const Tech = () => {
     return (
         <>
-            <p className="text-center text-4xl font-bold text-gray-500 mt-5">Tech Stack</p>
+            <p className="text-center text-4xl font-bold text-gray-500 mt-8">Tech Stack</p>
             <div className="grid grid-cols-2 grid-rows-2 sm:flex m-auto sm:justify-evenly w-full sm:w-3/5 mt-10">
 
-                <div className="m-auto mt-2">
+                <div className="m-auto mt-2 sm:m-0">
                     <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
                         <div className="w-24 h-24 sm:w-28 sm:h-28 rounded-full bg-white flex justify-center items-center">
                             <img src={html} alt="html logo" className="w-16" />
@@ -21,7 +21,7 @@ export const Tech = () => {
                     <p className="text-center pt-2 text-lg font-bold">HTML5</p>
                 </div>
 
-                <div className="m-auto">
+                <div className="m-auto sm:m-0">
                     <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
                         <div className="w-24 h-24 sm:w-28 sm:h-28  rounded-full bg-white flex justify-center items-center">
                             <img src={CSS} alt="css logo" className="w-12" />
@@ -30,7 +30,7 @@ export const Tech = () => {
                     <p className="text-center pt-2 text-lg font-bold">CSS3</p>
                 </div>
 
-                <div className="m-auto">
+                <div className="m-auto sm:m-0">
                     <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
                         <div className="w-24 h-24 sm:w-28 sm:h-28  rounded-full bg-white">
                             <img src={JavaScript} alt="js logo" />
@@ -39,7 +39,7 @@ export const Tech = () => {
                     <p className="text-center pt-2 text-lg font-bold">JAVASCRIPT</p>
                 </div>
 
-                <div className="m-auto">
+                <div className="m-auto sm:m-0">
                     <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
                         <div className="w-24 h-24 sm:w-28 sm:h-28  rounded-full bg-white">
                             <img src={ReactJS} alt="react js logo" className="p-2" />
@@ -47,11 +47,12 @@ export const Tech = () => {
                     </div>
                     <p className="text-center pt-2 text-lg font-bold">REACT JS</p>
                 </div>
+
             </div>
 
-            <div className="grid grid-cols-2 grid-rows-1 sm:flex m-auto sm:justify-evenly w-full sm:w-1/3 mt-4 mb-10">
+            <div className="grid grid-cols-2 grid-rows-1 sm:flex sm:justify-evenly w-full m-auto sm:w-2/6 mt-4 mb-10">
 
-                <div className="m-auto mt-2">
+                <div className="m-auto sm:m-0">
                     <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
                         <div className="w-24 h-24 sm:w-28 sm:h-28 rounded-full bg-white flex justify-center items-center">
                             <img src={Redux} alt="html logo" className="w-16" />
@@ -60,7 +61,7 @@ export const Tech = () => {
                     <p className="text-center pt-2 text-lg font-bold">REDUX</p>
                 </div>
 
-                <div className="m-auto">
+                <div className="m-auto sm:m-0">
                     <div className="w-32 h-32 sm:w-36 sm:h-36 rounded-full p-4 bg-green-700 ">
                         <div className="w-24 h-24 sm:w-28 sm:h-28  rounded-full bg-white flex justify-center items-center">
                             <img src={Tailwind} alt="css logo" className="w-24" />
@@ -70,7 +71,7 @@ export const Tech = () => {
                 </div>
 
             </div>
-            <hr className="bg-gray-800" />
+            <hr className="h-1 bg-gray-200" />
         </>
     )
 }
