40f22b1d94ef61f5c3cd008cfe730bd0e10067e0
agarwalchirag <agarwalchirag140@gmail.com>
2021-08-05 21:04:23 +0530

    Again Modification in Achievements

--- src/Components/Achievments.jsx
+++ src/Components/Achievments.jsx
@@ -10,7 +10,7 @@ export const Achievments = () => {
                     <div className="w-48 h-48 sm:w-52 sm:h-52 rounded-full p-4 bg-green-700 ">
                         <div className="w-40 h-40 sm:w-44 sm:h-44 rounded-full bg-white flex justify-center items-center">
                             {/* <img src={html} alt="html logo" className="w-16" /> */}
-                            <p className="font-bold text-4xl">1200+ hrs</p>
+                            <p className="font-bold text-3xl">1200+ hrs</p>
                         </div>
                     </div>
                     <p className="text-center pt-2 text-2xl font-bold">Coding</p>
@@ -20,7 +20,7 @@ export const Achievments = () => {
                     <div className="w-48 h-48 sm:w-52 sm:h-52 rounded-full p-4 bg-green-700 ">
                         <div className="w-40 h-40 sm:w-44 sm:h-44 rounded-full bg-white flex justify-center items-center">
                             {/* <img src={CSS} alt="css logo" className="w-12" /> */}
-                            <p className="font-bold text-4xl">100+ hrs</p>
+                            <p className="font-bold text-3xl">100+ hrs</p>
                         </div>
                     </div>
                     <p className="text-center pt-2 text-2xl font-bold">SoftSkills</p>
@@ -30,7 +30,7 @@ export const Achievments = () => {
                     <div className="w-48 h-48 sm:w-52 sm:h-52 rounded-full p-4 bg-green-700 ">
                         <div className="w-40 h-40 sm:w-44 sm:h-44 rounded-full bg-white flex justify-center items-center">
                             {/* <img src={JavaScript} alt="js logo" /> */}
-                            <p className="font-bold text-4xl ">100+</p>
+                            <p className="font-bold text-3xl ">100+</p>
                         </div>
                     </div>
                     <p className="text-center pt-2 text-2xl font-bold">DSA Problems</p>
@@ -40,7 +40,7 @@ export const Achievments = () => {
                     <div className="w-48 h-48 sm:w-52 sm:h-52 rounded-full p-4 bg-green-700 ">
                         <div className="w-40 h-40 sm:w-44 sm:h-44 rounded-full bg-white flex justify-center items-center">
                             {/* <img src={ReactJS} alt="react js logo" className="p-2" /> */}
-                            <p className="font-bold text-4xl">10+</p>
+                            <p className="font-bold text-3xl">10+</p>
                         </div>
                     </div>
                     <p className="text-center pt-2 text-2xl font-bold">Projects</p>
