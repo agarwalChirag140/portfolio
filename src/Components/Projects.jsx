fd9fc71f8139118890963b400b5356dbe6f1334d
agarwalchirag <agarwalchirag140@gmail.com>
2021-08-05 19:32:07 +0530

    Modification on projects is complete

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -14,18 +14,19 @@ export const Projects = () => {
     return (
         <>
             <p className="text-center text-4xl font-bold text-gray-500 pt-10 pb-8">PROJECTS</p>
-            <div className="sm:flex sm:w-3/5 sm:m-auto">
+            <div className="md:w-11/12 lg:w-5/6 xl:flex xl:w-3/5 sm:m-auto h-4/5">
+
                 {/* Covid Tracker App */}
-                <div className="sm:w-1/3 m-2 shadow-2xl bg-gray-100">
-                    <img src={covid} alt="screenshot of covid" className="w-full p-2 mb-3" />
+                <div className="xl:w-1/3 m-2 shadow-2xl bg-gray-100">
+                    <img src={covid} alt="screenshot of covid" className="w-full h-52 md:h-96 xl:h-48 p-2 mb-2" />
                     <hr/>
-                    <h1 className="text-center text-2xl font-bold pt-2">Covid Tracker App</h1>
-                    <p className="text-center text-lg p-1 pt-4">In this application, we track the CoronaVirus Cases, Recovery, and Death through the number as well asthrough the map.</p>
-                    <div className="flex w-3/5 m-auto justify-between mb-8 mt-4">
+                    <h1 className="text-center text-3xl xl:text-2xl font-bold pt-2">Covid Tracker App</h1>
+                    <p className="text-center p-1 pt-4 h-56 text-base md:text-lg xl:text-base">In this application, we track the CoronaVirus Cases, Recovery, and Death through the number as well asthrough the map.</p>
+                    <div className="flex w-3/5 md:w-2/6 lg:w-1/4 xl:w-3/5 m-auto justify-between mb-8 mt-4">
                         <div className="flex">
                             <a href="https://github.com/agarwalchirag140/covid-tracker-app" target="_blank" className="flex">
                                 <img src={github} alt="github logo" className="w-6" />
-                                <p className="ml-1">Github</p>
+                                <p className="ml-1 ">Github</p>
                             </a>
                         </div>
                         <div className="flex">
@@ -36,22 +37,23 @@ export const Projects = () => {
                         </div>
                     </div>
                     {/* Tech Stack */}
-                    <div className="flex w-3/5 m-auto justify-evenly">
+                    <div className="flex w-3/4 md:w-2/4 lg:w-2/5 xl:w-3/5 m-auto justify-evenly">
                         <img src={ReactApp} alt="react logo" className="w-12 h-12 mb-2" />
                         <img src={Tailwind} alt="tailwind logo" className="w-12 h-12" />
                     </div>
                 </div>
+
                 {/* Netflix Clone */}
-                <div className="sm:w-1/3 mt-4  sm:m-2 shadow-2xl bg-gray-100">
-                    <img src={netflix} alt="screenshot of covid" className="w-full p-2 mb-3" />
+                <div className="xl:w-1/3 m-2 shadow-2xl bg-gray-100">
+                    <img src={netflix} alt="screenshot of covid" className="w-full h-52 md:h-96 xl:h-48 p-2 mb-2" />
                     <hr/>
-                    <h1 className="text-center text-2xl font-bold pt-2">Netflix Clone</h1>
-                    <p className="text-center text-lg p-1 pt-4">This is the complete clone of the Netflix website. On this website, the user can create an account, and if it's successful, then after the login, the user can see the trailer of different types of movies, web series, and documentaries.</p>
-                    <div className="flex w-3/5 m-auto justify-between mb-8 mt-4">
+                    <h1 className="text-center text-3xl xl:text-2xl font-bold pt-2">Netflix Clone</h1>
+                    <p className="text-center text-base md:text-lg xl:text-base p-1 pt-4 h-56">This is the complete clone of the Netflix website. On this website, the user can create an account, and if it's successful, then after the login, the user can see the trailer of different types of movies, web series, and documentaries.</p>
+                    <div className="flex w-3/5 md:w-2/6 lg:w-1/4 xl:w-3/5 m-auto justify-between mb-8 mt-4">
                         <div className="flex">
                             <a href="https://github.com/agarwalchirag140/netflix_clone" target="_blank" className="flex">
                                 <img src={github} alt="github logo" className="w-6" />
-                                <p className="ml-1">Github</p>
+                                <p className="ml-1 ">Github</p>
                             </a>
                         </div>
                         <div className="flex">
@@ -62,7 +64,7 @@ export const Projects = () => {
                         </div>
                     </div>
                     {/* Tech Stack */}
-                    <div className="flex w-3/5 m-auto justify-between mb-3">
+                    <div className="flex w-3/4 md:w-2/4 lg:w-2/5 xl:w-3/5  m-auto justify-between mb-3">
                         <img src={ReactApp} alt="react logo" className="w-12 h-12 mb-2" />
                         <img src={Redux} alt="redux logo" className="w-12 h-12" />
                         <img src={Tailwind} alt="tailwind logo" className="w-12 h-12" />
@@ -70,16 +72,16 @@ export const Projects = () => {
                     </div>
                 </div>
                 {/* Instagram Clone */}
-                <div className="sm:w-1/3 mt-4 sm:m-2 shadow-2xl bg-gray-100">
-                    <img src={instagram} alt="screenshot of covid" className="w-full p-2 mb-3" />
+                <div className="xl:w-1/3 m-2 shadow-2xl bg-gray-100">
+                    <img src={instagram} alt="screenshot of covid" className="w-full h-52 md:h-96 xl:h-48 p-2 mb-2" />
                     <hr/>
-                    <h1 className="text-center text-2xl font-bold pt-2">Instagram Clone</h1>
-                    <p className="text-center text-lg p-1 pt-4">This is the complete clone of Instagram. In this clone, the user can create an account, if it's successful, then after the login, the User can create a post, the user can like a post, user can add a comment on the post, and also user can see other users post on the homepage of Instagram.</p>
-                    <div className="flex w-3/5 m-auto justify-between mb-8 mt-4">
+                    <h1 className="text-center text-3xl xl:text-2xl font-bold pt-2">Instagram Clone</h1>
+                    <p className="text-center text-base md:text-lg xl:text-base p-1 pt-4 h-56">This is the complete clone of Instagram. In this clone, the user can create an account, if it's successful, then after the login, the User can create a post, the user can like a post, user can add a comment on the post, and also user can see other users post on the homepage of Instagram.</p>
+                    <div className="flex w-3/5 md:w-2/6 lg:w-1/4 xl:w-3/5 m-auto justify-between mb-8 mt-4">
                         <div className="flex">
                             <a href="https://github.com/agarwalchirag140/Instagram_clone" target="_blank" className="flex">
                                 <img src={github} alt="github logo" className="w-6" />
-                                <p className="ml-1">Github</p>
+                                <p className="ml-1 ">Github</p>
                             </a>
                         </div>
                         <div className="flex">
@@ -90,7 +92,7 @@ export const Projects = () => {
                         </div>
                     </div>
                     {/* Tech Stack */}
-                    <div className="flex w-3/5 m-auto justify-between mb-3">
+                    <div className="flex w-3/4 md:w-2/4 lg:w-2/5 xl:w-3/5  m-auto justify-between mb-3">
                         <img src={ReactApp} alt="react logo" className="w-12 h-12 mb-2" />
                         <img src={Redux} alt="redux logo" className="w-12 h-12" />
                         <img src={Tailwind} alt="tailwind logo" className="w-12 h-12" />
@@ -99,18 +101,19 @@ export const Projects = () => {
                 </div>
             </div>
             {/* 2nd row */}
-            <div className="sm:flex sm:w-3/5 sm:m-auto pt-8 mt-12">
+
+            <div className="md:w-11/12 lg:w-5/6 xl:flex xl:w-3/5 sm:m-auto pt-5">
                 {/* Image Search App*/}
-                <div className="sm:w-1/3 m-2 ml-1 shadow-2xl bg-gray-100">
-                    <img src={imageSearch} alt="screenshot of covid" className="w-full p-2 mb-3" />
+                <div className="xl:w-1/3 m-2 shadow-2xl bg-gray-100">
+                    <img src={imageSearch} alt="screenshot of covid" className="w-full h-52 md:h-96 xl:h-48 p-2 mb-2" />
                     <hr/>
-                    <h1 className="text-center text-2xl font-bold pt-2">Image Search App</h1>
-                    <p className="text-center text-lg p-1 pt-4">In this application, we can search the image of different thing. This application is building by using React JS and TailwindCSS</p>
-                    <div className="flex w-3/5 m-auto justify-between mb-8 mt-4">
-                        <div className="flex w-max">  
+                    <h1 className="text-center text-3xl xl:text-2xl font-bold pt-2">Image Search</h1>
+                    <p className="text-center text-base md:text-lg xl:text-base p-1 pt-4 h-56">In this application, we can search the image of different thing. This application is building by using React JS and TailwindCSS</p>
+                    <div className="flex w-3/5 md:w-2/6 lg:w-1/4 xl:w-3/5 m-auto justify-between mb-8 mt-4">
+                        <div className="flex">
                             <a href="https://github.com/agarwalchirag140/image_search" target="_blank" className="flex">
                                 <img src={github} alt="github logo" className="w-6" />
-                                <p className="ml-1">Github</p>
+                                <p className="ml-1 ">Github</p>
                             </a>
                         </div>
                         <div className="flex">
@@ -121,7 +124,7 @@ export const Projects = () => {
                         </div>
                     </div>
                     {/* Tech Stack */}
-                    <div className="flex w-3/5 m-auto justify-evenly">
+                    <div className="flex w-3/4 md:w-2/4 lg:w-2/5 xl:w-3/5  m-auto justify-evenly mb-3">
                         <img src={ReactApp} alt="react logo" className="w-12 h-12 mb-2" />
                         <img src={Tailwind} alt="tailwind logo" className="w-12 h-12" />
                     </div>
